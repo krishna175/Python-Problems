069d165daa828922607c2a0104bb19acb4eea4bf
HARRY <sathyanharikrishnan@gmail.com>
2021-08-18 06:18:03 +0530

    created a class variable

--- Part II/53_ClassObject.py
+++ Part II/53_ClassObject.py
@@ -1,4 +1,5 @@
 class student:   #created a class named student
+    teachers = ["Mahesh", "Girish","Shiv"] # creating a variable inside class
     pass         # pass is used to do nothing in python
 
 hemant = student()     # created an object of class student
@@ -14,3 +15,19 @@ nihal.name = "Nihal Tiwari"
 nihal.age = 20
 nihal.subjects = ["IOT","INS","Networking"]
 print(hemant.name,hemant.div, nihal.name,nihal.subjects,nihal.age)
+print(hemant.teachers)  # using the variable inside calss which is common for all the objects
+print(nihal.teachers)
+print(student.teachers)
+
+student.teachers = ["Mahesh", "Girish","Shiv", "Drahsti", "Neha"]
+nihal.teachers = ["Mahesh", "Girish","Shiv", "Drahsti"]
+hemant.teachers  = ["Mahesh", "Girish","Shiv","Neha"]
+
+
+print(hemant.teachers)  # using the variable inside calss which is common for all the objects
+print(nihal.teachers)
+print(student.teachers)
+print(nihal.teachers)
+
+print(hemant.__dict__)
+print(nihal.__dict__)
